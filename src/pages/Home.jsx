45aa1d800afa83941529aa332ac7afd9836f897b
Oliverizzle <trigmogi@gmail.com>
2020-04-25 14:42:06 -0500

    Things are in place but it's not great

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Howl, Howler } from 'howler';
 import { PitchShifter } from 'soundtouchjs';
 
 class Home extends Component {
-    state = { data: -1, cycleFile: ["/im.mp3", "/pickle.mp3", "/rick.mp3"], pitch: 1  };
+    state = { data: -1, cycleFile: ["/im.mp3", "/pickle.mp3", "/rick.mp3"], pitch: 1 };
 
     constructor(props) {
         super(props);
@@ -11,11 +10,17 @@ class Home extends Component {
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
         this.keyDown = this.keyDown.bind(this);
-        this.keyUp = this.keyUp.bind(this);
     }
 
+
+
     keyDown(event) {
-        
+        let json = require('./newworld.json')
+        // console.log(json)
+
+        let scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+        let midiNumber = 64;
+
 
         let cycle = this.state.data;
         if (cycle >= 2) {
@@ -25,26 +30,38 @@ class Home extends Component {
             cycle = cycle + 1;
         }
 
-        this.setState({cycleFile: this.state.cycleFile, data: cycle, pitch: this.state.pitch + .05 });
-        console.log(event)
+        this.setState({ cycleFile: this.state.cycleFile, data: cycle, pitch: this.state.pitch + .05 });
+        // console.log(event)
         if (event.key === "a") {
-            console.log("PRessed a")
             this.shiftPitch(this.state.pitch, "/rick.wav").send();
         }
         else if (event.key === "s") {
             this.shiftPitch(1, "/rick.wav").send();
         }
         else if (event.key === "d") {
+            console.log("hiff")
             this.shiftPitch(1, "/rick.wav", 5).send();
+        }
+        else if (event.key === "f") {
+            let x = 0;
+            json.tracks[0].notes.forEach(element => {
+                // console.log(element)
+                // console.log(1 - (.05 * (midiNumber - element.midi)));
+                // console.log(element.time*1000)
+
+                setTimeout(() => {
+                    this.shiftPitch(1 - (.05 * (midiNumber - element.midi)), "/rick.wav", element.duration).send();
+                }, element.time * 1000);
+
+
+            })
+
+        }
+        else if (event.key === "h") {
+            this.getDuration("/rick.mp3")
         }
 
     }
-
-    keyUp(event) {
-        console.log("KEY UPss")
-
-    }
-
 
     shiftPitch(pitch, file, duration) {
         var context = new AudioContext(), request = new XMLHttpRequest();
@@ -52,29 +69,31 @@ class Home extends Component {
         request.responseType = "arraybuffer";
         request.onload = function () {
             context.decodeAudioData(request.response, onDecoded);
+
         };
         function onDecoded(buffer) {
+
+            // console.log(buffer.length)
             var bufferSource = context.createBufferSource();
             const gainNode = context.createGain();
             bufferSource.buffer = buffer;
 
-            let shifter = new PitchShifter(context, buffer, 1024);
+            let shifter = new PitchShifter(context, buffer, 4096);
 
             shifter.on('play', (detail) => {
-                console.log(detail)
                 if (detail.percentagePlayed >= 100) {
                     shifter.disconnect();
+
                 }
             });
-            if(duration) {
-                shifter.tempo = shifter.duration/duration;
+            if (false) {
+                shifter.tempo = shifter.duration / duration;
             }
             else {
                 shifter.tempo = 1;
             }
-            
-            shifter.pitch = pitch;
 
+            shifter.pitch = pitch;
             shifter.connect(gainNode);
             gainNode.connect(context.destination);
             bufferSource.connect(context.destination);
@@ -82,6 +101,32 @@ class Home extends Component {
 
 
         return request;
+    }
+
+    async getDuration(file) {
+        var context = new AudioContext();
+        let request = new XMLHttpRequest();
+
+        request.open("GET", file, true);
+        request.responseType = "arraybuffer";
+        request.onload = function () {
+            // let x = await new Promise(resolve => {context.decodeAudioData(request.response, onDecoded)}, 2000);
+            let x = onDecoded;
+            console.log(onDecoded)
+
+        }
+
+        async function onDecoded(buffer) {
+            var bufferSource = context.createBufferSource();
+            bufferSource.buffer = buffer;
+            // console.log(buffer.duration)
+            // bufferSource.connect(context.destination);
+            // bufferSource.start();
+            return buffer.duration;
+        }
+        console.log(request.response)
+        let x = request.send();
+        console.log(request)
     }
 
     componentDidMount() {
@@ -96,16 +141,20 @@ class Home extends Component {
 
 
     handleClick() {
-
-        var soundPlayer = new Audio();
-        soundPlayer.src = "/pickle.mp3";
-        
-        soundPlayer.mozPreservesPitch = true;
-        soundPlayer.playbackRate = 0.1;
-        console.log(soundPlayer)
-        soundPlayer.play();
+        // let x = await this.getDuration("./rick.mp3")
+        this.playAudio();
     }
 
+
+    playAudio() {
+        console.log(this.getDuration("./rick.mp3"));
+        var soundPlayer = new Audio();
+        soundPlayer.src = "./rick.mp3";
+        soundPlayer.mozPreservesPitch = true;
+        soundPlayer.playbackRate = 1;
+        console.log(soundPlayer.length);
+        soundPlayer.play();
+    }
 
     render() {
 
